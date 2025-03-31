e95eb9accf9dd5c46ce135c781b363c22bcce10a
Lucas Mastrovito <lucas.mastrovito@epitech.eu>
2025-03-31 17:10:55 +0200

    up

--- portfolio/front/src/home/profile.js
+++ portfolio/front/src/home/profile.js
@@ -9,7 +9,7 @@ function Profile() {
                 <h2 id='job'>Développeur</h2>
                 <h3 id='description'>Spécialisé dans le développement 3D, je suis tout autant capable de créer des jeux vidéo que des expérience XR.</h3>
                 <h3 id='description'>Mes compétences ne s'arrêtant pas là, je suis également capable de développer un site web de A à Z, ainsi que des applications mobile ou logiciels.</h3>
-                <h3 id='description'>Voici mes projets:</h3>
+                <h3 id='description'>Voici mes projets :</h3>
             </div>
             <div id='home-btn-container'>
                 <HomeButton title='Pro' redirect='/pro'></HomeButton>
